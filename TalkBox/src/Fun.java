665f94f782cddd5c54e54079f9ab305065172e80
SeoHyun <SeoHyun@192.168.0.10>
2019-01-19 13:09:11 -0500

    test

--- TalkBox/src/Fun.java
+++ TalkBox/src/Fun.java
@@ -4,7 +4,7 @@ public class Fun {
 	public static void main(String[] args) {
 
 		System.out.println("Fun with Gittttt. Change here.");
-		System.out.println("Push!");
+		System.out.println("Hello world");
 	}
 
 }
