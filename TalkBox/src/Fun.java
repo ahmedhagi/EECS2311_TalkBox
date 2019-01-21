d5f2f26ab8bdb30112458d911e057970ace95d39
persaud29 <andrew.persaud13@gmail.com>
2019-01-21 12:20:47 -0500

    zcsxdsdc

--- TalkBox/src/Fun.java
+++ TalkBox/src/Fun.java
@@ -4,7 +4,12 @@ public class Fun {
 	public static void main(String[] args) {
 
 		System.out.println("Fun with Gittttt. Change here.");
-		System.out.println("Conflict string");
+		System.out.println("Hello world...AndrewTake2;");
+		System.out.println("Helo from Chandler");
+		System.out.println("Hello world...seohyun");
+		
+		//Change
+	
 	}
 
 }
