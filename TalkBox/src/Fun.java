0d49bd5019dfb08c49a96ebe26de35e473175018
aces1111 <aces1111@ea57.eecs.yorku.ca>
2019-01-21 12:11:47 -0500

    Commit lab2

--- TalkBox/src/Fun.java
+++ TalkBox/src/Fun.java
@@ -7,7 +7,7 @@ public class Fun {
 
 		System.out.println("Hello world...AndrewTake2;");
 		
-		System.out.println("Hello world... Chandler here!");
+		System.out.println("Hello world... Chandler here again");
 
 	}
 
