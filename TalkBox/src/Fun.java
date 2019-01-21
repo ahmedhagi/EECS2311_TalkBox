020c259d1f15affc5a076fe0b2edcaa77bbaa2ea
SeoHyun <SeoHyun@192.168.0.10>
2019-01-21 12:14:59 -0500

    merge

--- TalkBox/src/Fun.java
+++ TalkBox/src/Fun.java
@@ -4,8 +4,7 @@ public class Fun {
 	public static void main(String[] args) {
 
 		System.out.println("Fun with Gittttt. Change here.");
-
-		System.out.println("Hello world...seohyun2");
+		System.out.println("merge");
 	}
 
 }
