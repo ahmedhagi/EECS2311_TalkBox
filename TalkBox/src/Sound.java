73f6dfc657f26cdecdfef891f250b470ba9ee6f5
SeoHyun <SeoHyun@LAPTOP-RAFDCGBT>
2019-01-24 15:54:01 -0500

    change button to toggle and group the button

--- TalkBox/src/Sound.java
+++ TalkBox/src/Sound.java
@@ -1,4 +1,5 @@
-import java.awt.BorderLayout;
+
+
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -13,17 +14,29 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Sound extends JFrame {
 
 	private JPanel contentPane;
 	private File soundfilein;
+	private String name;
 	private AudioInputStream audioIn;
 	private Clip clip = null;
+	private ButtonGroup btngroup1;
+	/**
+	 * @wbp.nonvisual location=-38,419
+	 */
+	private final JTextField textField = new JTextField();
 
 	/**
 	 * Launch the application.
@@ -43,6 +56,7 @@ public class Sound extends JFrame {
 
 
 	public Sound() {
+		textField.setColumns(10);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 712, 531);
 		contentPane = new JPanel();
@@ -55,81 +69,73 @@ public class Sound extends JFrame {
 				Double.MIN_VALUE };
 		gbl_contentPane.rowWeights = new double[] { 1.0, 0.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				playMusic("data\\audio\\test1.wav");
-			}
-		});
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 1;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
-
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_1.gridx = 4;
-		gbc_btnNewButton_1.gridy = 1;
-		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
-
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
 		
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 2;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		GridBagConstraints gbc_btn1 = new GridBagConstraints();
+		gbc_btn1.insets = new Insets(0, 0, 5, 5);
+		gbc_btn1.gridx = 2;
+		gbc_btn1.gridy = 1;
+	
+		
+		JToggleButton btn1 = new JToggleButton("toggle 1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(btn1.isSelected()){
+					lblNewLabel.setText("button1 clicked");
+					playMusic("data\\audio\\test1.wav");
+	            }
+	            else {
+	            	//stop the music
+	          }
 			}
 		});
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_2.gridx = 6;
-		gbc_btnNewButton_2.gridy = 1;
-		contentPane.add(btnNewButton_2, gbc_btnNewButton_2);
+		contentPane.add(btn1, gbc_btn1);
+		
+		
+		JToggleButton btn2 = new JToggleButton("toggle2");
+		GridBagConstraints gbc_btn2 = new GridBagConstraints();
+		gbc_btn2.insets = new Insets(0, 0, 5, 5);
+		gbc_btn2.gridx = 4;
+		gbc_btn2.gridy = 1;
+		contentPane.add(btn2, gbc_btn2);
+		
+		JToggleButton btn3 = new JToggleButton("toggle 3");
+		GridBagConstraints gbc_btn3 = new GridBagConstraints();
+		gbc_btn3.insets = new Insets(0, 0, 5, 5);
+		gbc_btn3.gridx = 6;
+		gbc_btn3.gridy = 1;
+		contentPane.add(btn3, gbc_btn3);
 
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_3.gridx = 8;
-		gbc_btnNewButton_3.gridy = 1;
-		contentPane.add(btnNewButton_3, gbc_btnNewButton_3);
-
-		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
-		gbc_btnNewButton_4.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_4.gridx = 10;
-		gbc_btnNewButton_4.gridy = 1;
-		contentPane.add(btnNewButton_4, gbc_btnNewButton_4);
-
+		
+		JToggleButton btn4 = new JToggleButton("toggle4");
+		GridBagConstraints gbc_btn4 = new GridBagConstraints();
+		gbc_btn4.insets = new Insets(0, 0, 5, 5);
+		gbc_btn4.gridx = 8;
+		gbc_btn4.gridy = 1;
+		contentPane.add(btn4, gbc_btn4);
+		
+		
+		btngroup1 = new ButtonGroup();
+		addGroup(btngroup1,btn1);
+		addGroup(btngroup1,btn2);
+		addGroup(btngroup1,btn3);
+		addGroup(btngroup1,btn4);
+		
 	}
+	
+	public void addGroup(ButtonGroup btngroup,JToggleButton btn) {
+		btngroup.add(btn);
+	}
+	
 	private void playMusic(String SoundFile) {
 		try {
-			clip = AudioSystem.getClip();
+			clip = AudioSystem.getClip(); 
 		} catch (LineUnavailableException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -149,5 +155,6 @@ public class Sound extends JFrame {
 			ee.printStackTrace();
 		}
 	}
+
 
 }
