a09481663ca744a6ed9c22b579baf07a12c55a8e
persaud29 <andrew.persaud13@gmail.com>
2019-01-31 00:36:43 -0500

    .

--- TalkBox/src/TalkBox.java
+++ TalkBox/src/TalkBox.java
@@ -1,39 +1,43 @@
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-public class TalkBox implements TalkBoxConfiguration {
-
-	private int numberofAudioButton;
-	private int numberofAudiosets;
-	private String[][] AudioFileNames;
-	@Override
-	public int getNumberOfAudioButtons() {
-		// TODO Auto-generated method stub
-		return this.numberofAudioButton;
-	}
-
-	@Override
-	public int getNumberOfAudioSets() {
-		// TODO Auto-generated method stub
-		return this.numberofAudiosets;
-	}
-
-	@Override
-	public int getTotalNumberOfButtons() {
-		// TODO Auto-generated method stub
-		return this.numberofAudiosets*this.numberofAudioButton;
-	}
-
-	@Override
-	public Path getRelativePathToAudioFiles() {
-		// TODO Auto-generated method stub
-		return Paths.get("data//audio//");
-	}
-
-	@Override
-	public String[][] getAudioFileNames() {
-		// TODO Auto-generated method stub
-		return this.AudioFileNames;
-	}
-
+public class TalkBox {
+	
 }
+
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//public class TalkBox implements TalkBoxConfiguration {
+//
+//	private int numberofAudioButton;
+//	private int numberofAudiosets;
+//	private String[][] AudioFileNames;
+//	@Override
+//	public int getNumberOfAudioButtons() {
+//		// TODO Auto-generated method stub
+//		return this.numberofAudioButton;
+//	}
+//
+//	@Override
+//	public int getNumberOfAudioSets() {
+//		// TODO Auto-generated method stub
+//		return this.numberofAudiosets;
+//	}
+//
+//	@Override
+//	public int getTotalNumberOfButtons() {
+//		// TODO Auto-generated method stub
+//		return this.numberofAudiosets*this.numberofAudioButton;
+//	}
+//
+//	@Override
+//	public Path getRelativePathToAudioFiles() {
+//		// TODO Auto-generated method stub
+//		return Paths.get("data//audio//");
+//	}
+//
+//	@Override
+//	public String[][] getAudioFileNames() {
+//		// TODO Auto-generated method stub
+//		return this.AudioFileNames;
+//	}
+//
+//}
