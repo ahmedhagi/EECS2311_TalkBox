f2ab22a2160c9907e82364227e0df8737128cd58
Chandler Cabrera <cabrera.chandler@gmail.com>
2019-02-19 21:55:23 -0500

    Javadoc comments for the model components, to make it easier to follow

--- TalkBox/src/model/TalkBox.java
+++ TalkBox/src/model/TalkBox.java
@@ -7,15 +7,21 @@ import java.util.List;
 
 public class TalkBox implements TalkBoxConfiguration {
 
-	// object 안에 오디오 파일 갯수, 오디오 파일 셋, 코리스펀딩한 오디오 파일 이름들. equal to database
+	// Database where the audio sets are stored. It's a LinkedList (database) containing the 
 	private List<List<String>> audioFileNames;
 	
-
+	/**
+	 * Constructor shenanigans
+	 */
 	public TalkBox() {
 		audioFileNames = new LinkedList<>();
 		setDefaultAudioset();
 
 	}
+	
+	/**
+	 * Sets up the default audio-sets that are included in the TalkBox.
+	 */
 	public void setDefaultAudioset() {
 		  List<String> set1 = new LinkedList<>(); set1.add("hi.wav");
 		  set1.add("goodbye.wav"); set1.add("excuseme.wav"); set1.add("time.wav");
@@ -38,22 +44,37 @@ public class TalkBox implements TalkBoxConfiguration {
 	
 	}
 
+	/**
+	 * sets audioFileNames as the one provided
+	 * @param audioFilesNames
+	 */
 	public void setAudioFileNames(List<String> audioFilesNames) { //export 할때.
 		this.audioFileNames.add(audioFilesNames);
 
 	}
 
+	/**
+	 * @return the number of audio buttons
+	 * NOTE: currently defaulted at 5
+	 */
 	@Override
 	public int getNumberOfAudioButtons() { // ?
 		// TODO Auto-generated method stub
 		return 5;
 	}
 
+	/**
+	 * @return the number of audio sets
+	 */
 	@Override
 	public int getNumberOfAudioSets() { // make gui audiobutton set
 		return audioFileNames.size();
 	}
 
+	/**
+	 * Gets the size of audioFileNames
+	 * @return total number of buttons
+	 */
 	@Override
 	public int getTotalNumberOfButtons() {
 		// TODO Auto-generated method stub
@@ -66,14 +87,22 @@ public class TalkBox implements TalkBoxConfiguration {
 		return total;
 	}
 
+	/**
+	 * Gets the beginning of the file path where the audio sets are located
+	 * @return the Path where the audio sets are
+	 */
 	@Override
 	public Path getRelativePathToAudioFiles() {
 		return Paths.get("src//audio//");
 
 	}
 
+	/**
+	 * This method is used mainly to make GUI components. It iterates through each audio set in filename
+	 * @return fileNames
+	 */
 	@Override
-	public String[][] getAudioFileNames() { // make gui componets
+	public String[][] getAudioFileNames() {
 		int n = getNumberOfAudioSets();
 		String[][] fileNames = new String[n][];
 
@@ -87,22 +116,34 @@ public class TalkBox implements TalkBoxConfiguration {
 		return fileNames;
 	}
 	
+	/**
+	 * Adds audio file path to the specified index in audioFileNames 
+	 * @param idx the index to store the audio file
+	 * @param fileName the audio file to be added
+	 */
 	public void addAudio(int idx, String fileName) {
 		this.audioFileNames.get(idx).add(fileName);
 	}
 	
-	public List<String> getAudioList (int idx) {
+	/**
+	 * gets the audioList in audioFileNames at the specified index
+	 * @param idx the index to retrieve from
+	 * @return the audio set stored at idx
+	 */
+	public List<String> getAudioList(int idx) {
 		return this.audioFileNames.get(idx);
 	}
 	
+	/**
+	 * removes the audio within the audio set, located at the specified index, with the specified file name
+	 * @param idx the index of the audio set to be removed
+	 * @param fileName the audio file name to be removed
+	 */
 	public void removeAudio(int idx, String fileName) {
 		List<String> list = this.audioFileNames.get(idx);
 		int index = list.indexOf(fileName);
 		if (index >=0) {
 			list.remove(index);
 		}
-		
 	}
-	
-
 }
